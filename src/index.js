888581e9e569a978ac01e65660aeded6d9110ce3
Elmaz <dremdzhe@gmail.com>
2018-08-26 11:44:43 -0700

    project init and initial data fetch from youtube

---
+++ src/index.js
@@ -0,0 +1,55 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
+
+import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
+
+const API_KEY = 'AIzaSyBN6N8R9zViv5OzXcobYU5I6Tv41wa8n2g';
+
+
+
+//Create a new component. This component should produce some HTML
+
+
+class App extends Component{
+
+    constructor(props){
+        super(props);
+
+        this.state = {
+            videos: [],
+            selectedVideo: null
+        };
+
+        YTSearch({key: API_KEY, term: 'surfboards'}, (videos)=> {
+
+            this.setState({
+                videos:videos,
+                selectedVideo: videos[0]
+            });
+
+            //will result in this.setState({videos:videos});
+
+        });
+    }
+
+    render(){
+        return (
+            <div>
+
+                <SearchBar />
+                <VideoDetail video={this.state.selectedVideo}/>
+                <VideoList
+                    onVideoSelect={ selectedVideo => this.setState({selectedVideo}) }
+                    videos={this.state.videos} />
+            </div>
+        );
+    }
+
+};
+
+// Take this components generated HTML and put it on the page (in the DOM)
+
+ReactDOM.render(<App />, document.querySelector('.container'));
